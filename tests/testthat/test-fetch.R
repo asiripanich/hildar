f33637838fbd0a2353ca6b21e37140efd1d653d2
amarin <>
2019-09-19 14:13:14 +1000

    add test for fetch function

--- tests/testthat/test-fetch.R
+++ tests/testthat/test-fetch.R
@@ -6,3 +6,15 @@ test_that("get one year data", {
   expect_true(nrow(data) != 0)
   expect_true(ncol(data) != 0)
 })
+
+
+test_that("get missing data in one wave", {
+  selected_years <- 2011:2012
+  # Note:
+  # skdrvl: Currently holds a motor vehicle license
+  # only exists in wave 12 and 16
+  data <- hildar::fetch(years = selected_years, vars = "skdrvl")
+  expect_true(data[, data.table::uniqueN(wave)] == length(selected_years))
+  expect_true(nrow(data) != 0)
+  expect_true(ncol(data) != 0)
+})
