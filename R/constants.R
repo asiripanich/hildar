32b3192a58e7ae65af7d5e1ba3c5c5aafd99e944
amarin <>
2019-09-19 14:29:10 +1000

    remove private constants

--- R/constants.R
+++ R/constants.R
@@ -2,9 +2,7 @@
 #' @export HILDA
 HILDA <- list()
 HILDA$xwaveid <- "xwaveid"
-HILDA$data_path <- "/Users/amarin/Google Drive/PhD/projects/hildar/hilda-data"
 HILDA$household_population_weight <- "hhwth"
 HILDA$responding_person_population_weight <- "hhwtrp"
 HILDA$household_id <- "hhid"
 names(HILDA$xwaveid) <- "xwaveid"
-names(HILDA$data_path) <- "data_path"
