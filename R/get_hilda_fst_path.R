9a7c97707f11186ac42e17b839384df786cc3886
asiripanich <17020181+asiripanich@users.noreply.github.com>
2022-07-19 17:21:41 +0700

    - patch: `get_hilda_fst_path()` now looks for `HILDA_FST` in the global options before the .Renviron file.

--- R/get_hilda_fst_path.R
+++ R/get_hilda_fst_path.R
@@ -1,11 +1,16 @@
-#' Get the `HILDA_FST` environment variable
+#' Get the directory where HILDA fst files are stored.
 #'
-#' This returns an environment variable called `HILDA_FST` which is
-#' the pathname where HILDA fst files are stored.
+#' This function looks for `HILDA_FST` in global options and
+#' the `.Renviron` file, in this order, and returns the first
+#' one it finds.
 #'
-#' @return a pathname.
+#' @return a directory.
 #' @export
+#' get_hilda_fst_path()
 get_hilda_fst_path <- function() {
+  if (!is.null(getOption("HILDA_FST"))) {
+    return(getOption("HILDA_FST"))
+  }
   if (is.null(Sys.getenv("HILDA_FST"))) {
     stop("`HILDA_FST` doesn't exist.")
   }
