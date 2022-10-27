9a19fbb5bf764d57a05675c61b3ecc0664dc89ce
asiripanich <17020181+asiripanich@users.noreply.github.com>
2022-10-27 12:49:39 +1100

    refactor: `hil_dict()`

--- R/hil_dict.R
+++ R/hil_dict.R
@@ -1,3 +1,4 @@
+#' @template hilda_fst_dir
 #' @rdname hil_setup
 #' @export
 #' @examples
@@ -6,6 +7,7 @@
 #' hil_dict()
 #' }
 hil_dict <- function(hilda_fst_dir = NULL) {
+  hilda_fst_dir <- hilda_fst_dir %||% get_hilda_fst_dir()
   hilda_dict_path <- fs::path(hilda_fst_dir, "hil_dict.rds")
   checkmate::assert_file_exists(hilda_dict_path)
   readRDS(hilda_dict_path)
